cce8bb5c988e1506b38bc8721547b56bc0cd1a3b
Shashi Gowda <shashigowda91@gmail.com>
2014-08-19 20:06:21 +0530

    Setup IPython on status_started.Kernel event.

--- src/ijulia.js
+++ src/ijulia.js
@@ -1,48 +1,57 @@
 (function (IPython, $, Widgets) {
+    $(document).ready(function() {
+	Widgets.debug = false; // log messages etc in console.
 
-    Widgets.debug = true; // log messages etc in console.
-    var comm_manager = IPython.notebook.kernel.comm_manager;
+	// coordingate with Comm and redraw Signals
+	// XXX: Test using React here to improve performance
+	$([IPython.events]).on(
+	    'output_appended.OutputArea', function (event, type, value, md, toinsert) {
+		if (md && md.reactive) {
+		    // console.log(md.comm_id);
+		    toinsert.addClass("signal-" + md.comm_id);
+		    toinsert.data("type", type);
+		}
+	});
+	function initComm(evt, data) {
+	    var comm_manager = data.kernel.comm_manager;
+	    comm_manager.register_target("Signal", function (comm) {
+		comm.on_msg(function (msg) {
+		    //Widgets.log("message received", msg);
+		    var val = msg.content.data.value;
+		    $(".signal-" + comm.comm_id).each(function() {
+			var self = this;
+			var type = $(this).data("type");
+			if (val[type]) {
+			    var oa = new IPython.OutputArea();
+			    var toinsert = IPython.OutputArea.append_map[type].apply(
+				oa, [val[type], {}, $("<div/>")]
+			    );
+			    $(self).html(toinsert.html());
+			}
+		    });
+		});
+	    });
 
-    // coordingate with Comm and redraw Signals
-    // XXX: Test using React here to improve performance
-    $([IPython.events]).on(
-	'output_appended.OutputArea', function (event, type, value, md, toinsert) {
-    	    if (md && md.reactive) {
-		console.log(md.comm_id);
-    		toinsert.addClass("signal-" + md.comm_id);
-    		toinsert.data("type", type);
-    	    }
-    });
-
-    comm_manager.register_target("Signal", function (comm) {
-    	comm.on_msg(function (msg) {
-	    Widgets.log("message received", msg);
-    	    var val = msg.content.data.value;
-    	    $(".signal-" + comm.comm_id).each(function() {
-    		var self = this;
-    		var type = $(this).data("type");
-    		if (val[type]) {
-    		    var oa = new IPython.OutputArea();
-    		    var toinsert = IPython.OutputArea.append_map[type].apply(
-    			oa, [val[type], {}, $("<div/>")]
-    		    );
-    		    $(self).html(toinsert.html());
-    		}
-    	    });
-    	});
-    });
-
-    // Set up communication for Widgets
-    Widgets.commInitializer = function (widget) {
-	var comm = comm_manager.new_comm(
-	    "InputWidget", {widget_id: widget.id}
-	);
-	widget.sendUpdate = function () {
-	    // `this` is a widget here.
-	    // TODO: I have a feeling there's some
-	    //       IPython bookkeeping to be done here.
-	    Widgets.log("State changed", this, this.getState());
-	    comm.send({value: this.getState()});
+	    // Set up communication for Widgets
+	    Widgets.commInitializer = function (widget) {
+		var comm = comm_manager.new_comm(
+		    "InputWidget", {widget_id: widget.id}
+		);
+		widget.sendUpdate = function () {
+		    // `this` is a widget here.
+		    // TODO: I have a feeling there's some
+		    //       IPython bookkeeping to be done here.
+		    // Widgets.log("State changed", this, this.getState());
+		    comm.send({value: this.getState()});
+		}
+	    };
 	}
-    };
+
+	try {
+	    // try to initialize right away. otherwise, wait on the status_started event.
+	    initComm(undefined, IPython.notebook);
+	} catch (e) {
+	    $([IPython.events]).on('status_started.Kernel', initComm);
+	}
+    });
 })(IPython, jQuery, InputWidgets);
